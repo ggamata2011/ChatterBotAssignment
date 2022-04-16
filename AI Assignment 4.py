0cc024b0c9da74c97263a9ef82c866e6868e4a51
ggamata2011 <37355821+ggamata2011@users.noreply.github.com>
2022-04-15 23:05:56 -0700

    Added starter corpus and also added storage adapters for feedback learning

--- AI Assignment 4.py
+++ AI Assignment 4.py
@@ -2,28 +2,58 @@
 #Guiller Gamata
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.conversation import Statement
 
+#For use in learning feedback to help the bot learn
+def feedback():
+
+    feed = input()
+
+    if 'yes' in feed.lower():
+        return True
+    elif 'no' in feed.lower():
+        return False
+    else:
+        print('Enter "Yes" or "No"')
+        return feedback()
+#End function definition
 
 # Create a new chatBot
-chatbot = ChatBot('Assistant')
-
+chatbot = ChatBot('Assistant',logic_adapters =[
+    { 'import_path':'chatterbot.logic.BestMatch',
+      'default_response': 'I am not sure what you mean, I provide directions and general information only',
+      'maximum_similarity_threshold': 0.9}],
+      storage_adapter= 'chatterbot.storage.SQLStorageAdapter'
+    )    
 CorpusTrainer = ChatterBotCorpusTrainer(chatbot)
-#ListTrain.train("Hello There")
-CorpusTrainer.train("./Custom Conversation.yml")
-#"./CustomConversation.json"
-#"chatterbot.corpus.english","./Custom Conversation.yml",
+CorpusTrainer.train("./Greetings-Directions.yml","./General-Information.yml")
 
-
+print("Welcome to St.Rose Hospital main terminal, How may I help you?\n")
+    
 # Get a response to the input text 'I would like to book a flight.'
 while True:
     try:
+        print("You:")
         Query = input()
         response = chatbot.get_response(Query)
+        print("Assistant:")
         print(response)
+
+        #Verify for correct input
+        print('\nIs this a valid response?')
+        if feedback() is False:
+            print('please input the correct response')
+            text = input()
+            correct_response = Statement(text)
+            chatbot.learn_response(correct_response, Query)
+            print('Responses added to bot!')
+        #Response Correction done
 
     except(KeyboardInterrupt, EOFError, SystemExit):
         break
 
-
 #Export Data to json file
 #CorpusTrainer.export_for_training('./CustomConversation.json')
+
+
+    
